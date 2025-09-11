db3218749c0d7a18b6e547792ad8542ca03b910c
Akshaj Dev <akshajdevkv@gmail.com>
2025-09-11 19:04:28 +0530

    update ui of input section

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,7 +1,7 @@
 export default function Input({label,textarea, ...props}){
     return <>
-    <p>
-         <label>{label}</label>
+    <p className="flex flex-col gap-1 my-4">
+         <label className="text-sm font-bold uppercase">{label}</label>
          {textarea? <textarea {...props}/> : <input {...props}/> }
     </p>
         </>
