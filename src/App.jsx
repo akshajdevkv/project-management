2dc281d8c738d14a3dc72b1a2289d7188fd34651
Akshaj Dev <akshajdevkv@gmail.com>
2025-09-14 16:38:18 +0530

    fix value handling issue in newProject component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,50 @@
 import NewProject from "./components/NewProject";
+import NoProjectSelected from "./components/NoProjectSelected";
 import ProjectsSidebar from "./components/ProjectsSidebar";
-
+import {useState} from "react"
 export default function App(){
+  const [projectsState,setProjectsState]= useState({
+ 
+    selectedProjectId:undefined,
+    projects:[]
+
+  })
+  function handleStartAddProject(){
+    setProjectsState( prevState=>{
+        return{
+          ...prevState,
+          selectedProjectId:null,
+
+        }
+    })
+  }
+
+  function handleAddProject(projectData){
+      setProjectsState(prevState => {
+        const newProject ={
+            ...projectData,
+            id: Math.random()
+
+        }
+        return {
+          ...prevState,
+          selectedProjectId:undefined,
+          projects:[...prevState.projects,newProject]
+        }
+      });
+  }
+  console.log(projectsState.projects)
+  let content;
+  if(projectsState.selectedProjectId===null){
+    content = <NewProject onAdd={handleAddProject}/>
+
+  }else if(projectsState.selectedProjectId===undefined){
+    content = <NoProjectSelected onStartAddProject={handleStartAddProject}/>
+  }
   return  <>
     <main className="h-screen my-8 flex gap-8">
-      <ProjectsSidebar/>
-      <NewProject/>
+      <ProjectsSidebar onStartAddProject={handleStartAddProject} projects={projectsState.projects}/>
+       {content}
     </main>
   </>
 }
